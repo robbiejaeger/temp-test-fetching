4284c4ad264ebd21f6ae9a96ff209fa069138518
Robbie Jaeger <jaeger.rob@gmail.com>
2019-09-06 19:11:08 -0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {response: ''};
+  }
+
+  componentDidMount() {
+    const bodyInfo = {
+      name: 'Divad',
+      email: 'Divad@gmail.com',
+      password: 'divadspassword'
+    }
+    const options = {
+      method: "POST",
+      body: JSON.stringify(bodyInfo),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    fetch("http://localhost:3001/api/v1/users/", options)
+      .then(response => response.json())
+      .then(data => this.setState({response: JSON.stringify(data)}))
+      .catch(err => this.setState({response: JSON.stringify(err)}));
+
+    // const bodyInfo = {
+    //   email: 'Divad@gmail.com',
+    //   password: 'divadspassword'
+    // }
+
+    // const options = {
+    //   method: "POST",
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify(bodyInfo)
+    // }
+
+    // fetch("http://localhost:3001/api/v1/login/", options)
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>So Fetch</h1>
+        <p>{this.state.response}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
